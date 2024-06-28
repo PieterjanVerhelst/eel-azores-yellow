af821d69cbe6053f5a16732c05b531fb6f17497a
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2024-06-28 14:36:55 +0200

    update script from shads to eels

--- src/download_data.R
+++ src/download_data.R
@@ -14,49 +14,34 @@ my_con <- connect_to_etn("pieterjan.verhelst@inbo.be",
 my_con
 
 
-
 # 1. Download detection data ####
-data <- get_acoustic_detections(my_con, scientific_name = "Alosa fallax",
-                                animal_project_code = "2015_fint",
+data <- get_acoustic_detections(my_con, scientific_name = "Anguilla anguilla",
+                                animal_project_code = "2021_YEELAZ",
                                 limit = FALSE)
 
 # Select relevant columns
 data <- select(data, animal_project_code, scientific_name, date_time, acoustic_tag_id, station_name, acoustic_project_code, receiver_id, deploy_latitude, deploy_longitude)
 
-# Remove detections at station ak-x
-data <- data[!(data$station_name=="ak-x"),]
 
-
-# 2. Download shad meta-data ####
-shads <- get_animals(my_con, scientific_name = "Alosa fallax", 
-                     animal_project_code = "2015_fint")
+# 2. Download eel meta-data ####
+eels <- get_animals(my_con, scientific_name = "Anguilla anguilla", 
+                    animal_project_code = "2021_YEELAZ")
 
 # Select relevant columns
-shads <- select(shads, animal_project_code, scientific_name, acoustic_tag_id,
-                capture_date_time, capture_location, capture_latitude, capture_longitude, capture_method,
-                release_date_time, release_location, release_latitude, release_longitude,
-                length1_type, length1, length1_unit,
-                weight, weight_unit,
-                age, age_unit, sex, life_stage, 
-                tagging_type)
+eels <- select(eels, animal_project_code, scientific_name, acoustic_tag_id,
+               capture_date_time, capture_location, capture_latitude, capture_longitude, capture_method,
+               release_date_time, release_location, release_latitude, release_longitude,
+               length1_type, length1, length1_unit,
+               length2_type, length2, length2_unit,
+               length3_type, length3, length3_unit,
+               length4_type, length4, length4_unit,
+               weight, weight_unit,
+               age, age_unit, sex, life_stage, 
+               treatment_type)
 
 
 # 3. Download deployment positions ####
-network_projects <- c("lifewatch",
-                      "apelafico",
-                      "leopold",
-                      "cpodnetwork",
-                      "dijle",
-                      "albert",
-                      "bpns",
-                      "demer",
-                      "ws1",
-                      "ws2",
-                      "ws3",
-                      "zeeschelde",
-                      "2019_Grotenete",
-                      "SWIMWAY_2021",
-                      "FISHINTEL")
+network_projects <- c("AZO")
 
 deployments <- get_acoustic_deployments(my_con, acoustic_project_code = network_projects, open_only = FALSE)
 deployments$station_name <- factor(deployments$station_name)
@@ -72,6 +57,7 @@ deployments <- select(deployments, acoustic_project_code, station_name, receiver
 
 # write csv
 write.csv(data, "raw_detection_data.csv")
-write.csv(shads, "shad_meta_data.csv")
+write.csv(eels, "flores_eels_meta_data.csv")
 write.csv(deployments, "deployments.csv")
+
 
