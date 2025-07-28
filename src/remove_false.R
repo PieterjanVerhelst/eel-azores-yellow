6deb6c02ae9e575d026641084644100c559558b7
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2025-07-28 20:18:25 +0200

    remove potential false detections

--- src/remove_false.R
+++ src/remove_false.R
@@ -18,3 +18,22 @@ data <- data[!(data$acoustic_project_code == "FISHOWF"),]
 data <- data[!(data$acoustic_project_code == "FISHINTEL"),]
 data <- data[!(data$acoustic_project_code == "GTN"),]
 data <- data[!(data$acoustic_project_code == "PTN/ATLAZUL"),]
+
+
+# Remove likely false detections at station 155 FLO CRUZ
+data <- data[!(data$acoustic_tag_id == "A69-1303-2692" & data$station_name == "155 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2698" & data$station_name == "155 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2699" & data$station_name == "155 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2706" & data$station_name == "155 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2707" & data$station_name == "155 FLO CRUZ"),]
+
+# Remove likely false detections at station 152 FLO CRUZ
+data <- data[!(data$acoustic_tag_id == "A69-1303-2713" & data$station_name == "152 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2714" & data$station_name == "152 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2715" & data$station_name == "152 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2716" & data$station_name == "152 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2717" & data$station_name == "152 FLO CRUZ"),]
+data <- data[!(data$acoustic_tag_id == "A69-1303-2719" & data$station_name == "152 FLO CRUZ"),]
+
+
+
